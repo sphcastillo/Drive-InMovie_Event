db0b3a2bf63ba28bff03d3f4e0f954448dec85ff
Sophia Castillo <Sphcastillo@gmail.com>
2021-05-17 15:19:48 -0700

    adding useState

--- accordion/src/components/Accordion.js
+++ accordion/src/components/Accordion.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Data } from './Data';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const AccordionSection = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    height: 100vh;
+    background: #fff;
+`;
+
+const Container = styled.div`
+
+    position: absolute;
+    top: 30%;
+    box-shadow: 2px 10px 25px 1px rgba(153, 153, 153, 0.3);
+`;
+
+const Wrap = styled.div`
+
+    background: #272727;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+
+    h1 {
+        padding: 2rem;
+        font-size: 2rem;
+    }
+    
+`;
 
 function Accordion() {
     return (
-        <div>
-            <h1>This is an accordion</h1>
-        </div>
+        <IconContext.Provider
+            value = {{ color: '#00FFB9', size: '25px'}}
+        >
+            <AccordionSection>
+                <Container>
+                    {Data.map((item, index) => {
+                        return (
+                            <>
+                                <Wrap>
+                                    <h1>{item.question}</h1>
+                                </Wrap>
+                                    <p>{item.answer}</p>
+                            </>
+                        )
+                    })}
+                </Container>
+            </AccordionSection>
+        
+        </IconContext.Provider>
     )
 }
 
---
+++ accordion/src/components/Data.js
@@ -0,0 +1,44 @@
+export const Data = [
+    {
+        question: "Why did the bike fall over?",
+        answer: "It was two tired."
+    },
+    {
+        question: "What did the policeman say to his belly button?",
+        answer: "You’re under a vest."
+    },
+    {
+        question: "Why did the man get hit by a bike every day?",
+        answer: "He was stuck in a vicious cycle."
+    },
+    {
+        question: "What’s the difference between the bird flu and the swine flu?",
+        answer: "One requires tweetment and the other an oinkment."
+    },
+    {
+        question: "Why do people say “break a leg” when you go on stage?",
+        answer: "Because every play has a cast."
+    },
+    {
+        question: "Why are there gates around cemeteries?",
+        answer: "Because people are dying to get in."
+    },
+    {
+        question: "Why shouldn’t you write with a broken pen?",
+        answer: "Because it’s pointless."
+    },
+    {
+        question: "Why did the bike fall over?",
+        answer: "It was two tired."
+    },
+    {
+        question: "What did the yoga instructor say when her landlord tried to evict her?",
+        answer: "Namaste."
+    },
+    {
+        question: "What do you call fake spaghetti?",
+        answer: "An im-pasta."
+    },
+
+
+]
